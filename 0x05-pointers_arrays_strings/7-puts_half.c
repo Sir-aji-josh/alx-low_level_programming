5d6a2644c6a82a85c83317bd3c6b53eefce0a68c
root <joshman.aji@gmail.com>
2023-03-27 15:01:09 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string followed by \n
+ * @str: string to be printed
+ * Return: limit 
+ */
+void puts_half(char *str)
+{
+	int lim, j, i;
+
+	lim = 0;
+
+	while (str[lim] != '\0')
+	{
+		lim++;
+	}
+
+	if (lim % 2 == 0)
+	{
+		for (i = lim / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (lim % 2)
+	{
+		for (j = (lim - 1) / 2; j < lim - 1; j++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
