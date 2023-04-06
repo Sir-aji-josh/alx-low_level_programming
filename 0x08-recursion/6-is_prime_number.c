e4dcd5ed7bc5e561f9dd3dff8d96ca200426c62c
Sir-aji-josh <joshman.aji@gmail.com>
2023-04-06 05:28:39 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,31 +3,34 @@
 /**
  * is_prime - detects if an input number is a prime number.
  * @n: input number.
- * @c: iterator.
+ * @c: input value.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_prime(int n, int c) 
+
+int is_prime(int n, int c)
 {
-	if (c == 1 || c == n)
-		return (0);
+        if (c == 1 || c == n)
+                return (0);
 
-	if (n % c  == 0)
-		return (1);
+        if (n % c  == 0)
+                return (1);
 
-	return (is_prime(n, c - 1));
+        return (is_prime(n, c - 1));
 }
+
 /**
  * is_prime_number - detects if an input number is a prime number.
  * @n: input number.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
+
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
+        if (n <= 1)
+                return (0);
 
-	if (n ==  2)
-		return (1);
+        if (n ==  2)
+                return (1);
 
-	return (!is_prime(n, n - 1));
+        return (!is_prime(n, n - 1));
 }
