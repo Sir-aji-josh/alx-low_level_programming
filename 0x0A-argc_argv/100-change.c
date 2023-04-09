7af589e76295d0856361e3b6a34f8f090a745500
Sir-aji-josh <joshman.aji@gmail.com>
2023-04-08 18:58:46 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int cents, xcoins = 0;
 
 	if (argc == 1 || argc > 2)
 	{
@@ -31,8 +31,8 @@ int main(int argc, char *argv[])
 			cents -= 2;
 		else if (cents >= 1)
 			cents -= 1;
-		ncoins += 1;
+		xcoins += 1;
 	}
-	printf("%d\n", ncoins);
+	printf("%d\n", xcoins);
 	return (0);
 }
