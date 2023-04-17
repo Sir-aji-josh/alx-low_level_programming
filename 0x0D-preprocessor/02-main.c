0922d297c8849bc84f9893d9b732a9ffbedc25c3
Sir-aji-josh <joshman.aji@gmail.com>
2023-04-17 02:37:56 -0700

    task 2, 2-main.c Done

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - A function that prints the name of the file it was compiled form
+*
+* Return: void (0)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
