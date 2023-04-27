67300c730e2f2a0ba91b87c7e069996bfafa7299
Sir-aji-josh <joshman.aji@gmail.com>
2023-04-27 14:09:43 -0700

    100-first.c, task 5 done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void __attribute__ ((constructor))first(void);
+
+/**
+ * first - Print a sentence befor the main
+ * function is executed
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
