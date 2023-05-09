3675b3bce88be2006c51145a261041a4e3e33420
Sir-aji-josh <joshman.aji@gmail.com>
2023-05-08 19:10:33 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,24 +14,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o_open, w_write, len = 0;
+        int o_open, w_write, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+        if (filename == NULL)
+                return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+        if (text_content != NULL)
+        {
+                for (len = 0; text_content[len];)
+                        len++;
+        }
 
-	o_open = open(filename, O_WRONLY | O_APPEND);
-	w_write = write(o_open, text_content, len);
+        o_open = open(filename, O_WRONLY | O_APPEND);
+        w_write = write(o_open, text_content, len);
 
-	if (o_open == -1 || w_write -1)
-		return (-1);
+        if (o_open == -1 || w_write -1)
+                return (-1);
 
-	close(o_open);
+        close(o_open);
 
-	return (1);
+        return (1);
 }
