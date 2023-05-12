7e0cedf9c33f6eabb9b9f8eb840d3166ccc15b8d
Sir-aji-josh <joshman.aji@gmail.com>
2023-05-12 16:41:36 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Prototypes For Task 1; 2; 3; */
+/* Prototypes For Task 0,1,2; */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
