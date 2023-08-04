9b2b348b45c16be3f27df522cabd5eb84ef1840f
Sir-aji-josh <joshman.aji@gmail.com>
2023-08-03 18:41:03 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,7 @@
  * flip_bits - Returns The Number of Bits You Would
  * Need To Flip To Get From One Number To Another
  * @n: Words Number One.
- * @o: Words Number Two.
+ * @m: Words Number Two.
  *
  * Return: Number Of Bits.
  */
@@ -12,9 +12,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned int nbits;
 
-for (nbits = 0; n || o; n >>= 1, o >>= 1)
+for (nbits = 0; n || m; n >>= 1, m >>= 1)
 {
-if ((n & 1) != (o & 1))
+if ((n & 1) != (m & 1))
 nbits++;
 }
 return (nbits);
